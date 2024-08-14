f57f2682d8b4b8266afa6581f0bced93c41fc61f
Yuan Si <do4suki@gmail.com>
2024-08-14 09:22:43 +0800

    Add axio as dependency
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ chown -R root:root /koishi
 if [ ! -e "/koishi/package.json" ]; then
   cp -R /koishi_temp/* /koishi
   npm install
+  npm install axio
   sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
 fi
 
